0d0d705d9bc5fae9ef2dcca2478f739e397df0be
Chita <hidlove@126.com>
2014-11-25 18:57:33 +0800

    setTimeout test

--- cloud/app.js
+++ cloud/app.js
@@ -10,6 +10,21 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
+  setTimeout(
+  AV.Cloud.httpRequest,{
+  url: 'http://www.example.com/',
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  success: function(httpResponse) {
+    console.log(httpResponse.text);
+  },
+  error: function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+  }
+},1000
+            )
 });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
