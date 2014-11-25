1528d33cdde8614fbe5d5e70134676f5b8d946fa
Chita <hidlove@126.com>
2014-11-25 19:06:28 +0800

    fix http request bug

--- cloud/app.js
+++ cloud/app.js
@@ -10,8 +10,8 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
-  setTimeout(
-  AV.Cloud.httpRequest,{
+  setInterval(function (){
+  AV.Cloud.httpRequest({
   url: 'http://www.example.com/',
   timeout: 15000,
   headers: {
@@ -23,7 +23,7 @@ app.get('/hello', function(req, res) {
   error: function(httpResponse) {
     console.error('Request failed with response code ' + httpResponse.status);
   }
-},1000
+})},1000
             )
 });
 
